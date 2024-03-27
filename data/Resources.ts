f1ac2f86128f39ae0f44e73b24f8e200b9ef95db
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-03-27 12:01:10 +0000

    Updating resources

--- data/Resources.ts
+++ data/Resources.ts
@@ -1,32 +1,42 @@
 export const Resources = [
-    {
-        name: "Sustainable Web Design in 20 Lessons, by Michael Andersen",
-        link: "https://sustainablewww.org/news/sustainable-web-design-in-20-lessons-pre-order-now/",
-        type : "Book"
-    },
-    {
-        name: "Green Software for Practitioners (LFC131) - The Linux Foundation",
-        link: "https://training.linuxfoundation.org/training/green-software-for-practitioners-lfc131/",
-        type : "Training"
-    },
-    {
-        name: "Green Software Foundation",
-        link: "https://greensoftware.foundation/",
-        type : "Non-Profit Organisation"
-    },
-    {
-        name: "The Green Web Foundation",
-        link: "https://www.thegreenwebfoundation.org/",
-        type : "Carbon Checker"
-    },
-    {
-        name: "The Green Pages",
-        link: "https://thegreenpages.bima.co.uk/the-green-pages/",
-        type : "Resource List"
-    },
-    {
-        name: "Cloudflare Pages",
-        link: "https://pages.cloudflare.com/",
-        type : "Green Hosting"
-    }
-]
+  {
+    name: 'Sustainable Web Design in 20 Lessons, by Michael Andersen',
+    link: 'https://sustainablewww.org/news/sustainable-web-design-in-20-lessons-pre-order-now/',
+    type: 'Book',
+  },
+  {
+    name: 'Green Software for Practitioners (LFC131) - The Linux Foundation',
+    link: 'https://training.linuxfoundation.org/training/green-software-for-practitioners-lfc131/',
+    type: 'Training',
+  },
+  {
+    name: 'Green Software Foundation',
+    link: 'https://greensoftware.foundation/',
+    type: 'Non-Profit Organisation',
+  },
+  {
+    name: 'The Green Web Foundation',
+    link: 'https://www.thegreenwebfoundation.org/',
+    type: 'Carbon Checker',
+  },
+  {
+    name: 'The Green Pages',
+    link: 'https://thegreenpages.bima.co.uk/the-green-pages/',
+    type: 'Resource List',
+  },
+  {
+    name: 'Cloudflare Pages',
+    link: 'https://pages.cloudflare.com/',
+    type: 'Green Hosting',
+  },
+  {
+    name: 'Netlify',
+    link: 'https://www.netlify.com/',
+    type: 'Green Hosting',
+  },
+  {
+    name: 'Digital Ocean',
+    link: 'https://www.digitalocean.com/',
+    type: 'Green Hosting',
+  },
+];
